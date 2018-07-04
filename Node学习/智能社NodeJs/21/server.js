3b92137785e1f91f66a777dcc7d568aa856d4b56
zhangmiaochen <1923097096@qq.com>
2018-07-04 16:03:15 +0800

    第29课网站框架搭建

---
+++ Node学习/智能社NodeJs/21/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const static = require("express-static");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const mysql = require("mysql");
+const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
+const consolidate = require("consolidate");
+const ejs = require("ejs");
+const expressRoute = require("express-route");
+const multerObj = multer({ dest: "./static/upload" });
+var server = express();
+server.listen(800);
+//1.获取请求数据
+//get自带
+server.use(multerObj.any());
+//2.cookie、session
+server.use(cookieParser());
+(function() {
+    var keys = [];
+    for (var i = 0; i < 100000; i++) {
+        keys[i] = 'a_' + Math.random();
+    }
+    server.use(cookieSession({
+        name: 'sess_id',
+        keys: keys,
+        maxAge: 20 * 60 * 1000 //20MIN
+    }));
+})()
+//3.模板
+server.engine("html", consolidate.ejs);
+server.set("views", "template");
+server.set("view engin", "html");
+//4.route
+server.use("/article/", require("./route/1.js")());
+server.use("/blog/", require("./route/2.js")());
+
+//5.default:static
+server.use(static("./static/"));
